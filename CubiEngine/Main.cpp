6986f4c9666210bf37b84d7c1982118ead955c6e
jooh3444 <jooh3444@gmail.com>
2024-09-30 17:02:03 +0900

    feat: add and link library SDL2, imgui. add simple SDL2 sources.

--- CubiEngine/Main.cpp
+++ CubiEngine/Main.cpp
@@ -1,5 +1,45 @@
-#include <stdio.h>
-int main()
-{
-    printf("Hello World!\n");
+#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_SHOWN);
+
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+            else if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    running = false;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(SDL_GetRenderer(window), 0, 0, 0, 255);
+        SDL_RenderClear(SDL_GetRenderer(window));
+        SDL_RenderPresent(SDL_GetRenderer(window));
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ cmake -S . -B Build
 ```
 
 + Then open Build/CubiEngine.sln and build solution.
+
+# Reference Projects
+[Helios](https://github.com/rtarun9/Helios/tree/master) By rtarun9
